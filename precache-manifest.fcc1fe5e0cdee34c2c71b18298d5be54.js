6f37ffae6120eecbc73799b1286d54e1c885d16a
waltrevino <27642702+Waltrevino@users.noreply.github.com>
2020-08-25 17:14:01 -0500

    Updates

---
+++ precache-manifest.fcc1fe5e0cdee34c2c71b18298d5be54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255aba7ba42512b9e74dc7586b465b09",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "0257f8834180a70bdaa0",
+    "url": "/react_portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6240303912ba9da13740",
+    "url": "/react_portfolio/static/js/2.0feb8754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_portfolio/static/js/2.0feb8754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0257f8834180a70bdaa0",
+    "url": "/react_portfolio/static/js/main.ede3956b.chunk.js"
+  },
+  {
+    "revision": "9516736bfd4120688efb",
+    "url": "/react_portfolio/static/js/runtime-main.aff8cc16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
