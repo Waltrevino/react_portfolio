890af85e31d85efff6fb6cd823f874dcc6ee13c0
waltrevino <27642702+Waltrevino@users.noreply.github.com>
2020-08-27 00:03:20 -0500

    update about me desc

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -20,10 +20,10 @@ function AboutContainer() {
                 <Card className="h-100 border-0">
                     <Card.Header className="text-dark bg-white"><h4>Brief Description</h4></Card.Header>
                     <Card.Body>
-                    <p className="lead">Software Engineer who loves creating and fixing things.  I enjoy transforming ideas into reality by using code and using the latest technology to build user friendly interactive webpages.  </p><p className="lead"> I am currently enrolled in the Full Stack Web Developer Bootcamp through the University of Texas at UT with graduation taking place at the end of August .</p>
-                    <p className="lead">I am anxious to start working on new projects.  I want to design and develop custom websites using the latest technologies that I have been learning, such as HTML5, CSS, JavaScript, React.js, jQuery, Bootstrap, APIs, AJAX, MySQL, MongoDB, Node.js, Express.js, and GitHub.   </p>
+                    <p className="lead">Software Engineer who loves creating and fixing things.  I enjoy transforming ideas into reality by using code and using the latest technology to build user friendly interactive webpages.  </p><p className="lead"> I am currently enrolled in the Full Stack Web Developer Bootcamp through the University of Texas at Austin with graduation taking place at the end of August .</p>
+                    <p className="lead">I am anxious to start working on new projects once my bootcamp ends..  I want to design and develop custom websites using the latest technologies that I have been learning, such as HTML5, CSS, JavaScript, React.js, jQuery, Bootstrap, APIs, AJAX, MySQL, MongoDB, Node.js, Express.js, Heroku, and GitHub.   </p>
                     <p className="lead">
-                        My dream job is to work for a company that values innovation and worklife balance. And I will add value to that company by bringing my strong collaboration skills and expertise I that have acquired throughout my work history. 
+                        My dream job is to work for a company that values innovation and worklife balance. And I will add value to that company by bringing my strong collaboration skills and expertise that I have acquired throughout my work history. 
                     </p>
                     </Card.Body>
                 </Card>
